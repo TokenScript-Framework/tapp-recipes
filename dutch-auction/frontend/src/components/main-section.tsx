41cfa123d1fbf34439b69420f408b07a308d675c
Justin Feng <realfengjia@foxmail.com>
2024-11-21 18:07:55 +0800

    chore: 🎸 add share on twitter for ongoing auction

--- dutch-auction/frontend/src/components/main-section.tsx
+++ dutch-auction/frontend/src/components/main-section.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
@@ -10,13 +11,18 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { MORCHI_NFT_CONTRACT } from '@/lib/constant';
+import {
+  CHAIN_ID,
+  DUTCH_AUCTION_ABI,
+  DUTCH_AUCTION_CONTRACT,
+  MORCHI_NFT_CONTRACT,
+} from '@/lib/constant';
 import { myNfts, MyNftToken } from '@token-kit/onchain';
 import { useCallback, useEffect, useState } from 'react';
-import { PublicClient } from 'viem';
-import { useAccount, usePublicClient } from 'wagmi';
-import { Loading } from './loading';
+import { PublicClient, zeroAddress } from 'viem';
+import { useAccount, usePublicClient, useReadContracts } from 'wagmi';
 import { CreateModal } from './create-modal';
+import { Loading } from './loading';
 
 export type MorchiMetadata = {
   name: string;
@@ -29,6 +35,10 @@ export function MainSection() {
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const [isLoadingNFTs, setIsLoadingNFTs] = useState(false);
   const [nfts, setNfts] = useState<MyNftToken[]>([]);
+  // { [tokenId: string]: onAuction }
+  const [auctionStatus, setAuctionStatus] = useState<Record<string, boolean>>(
+    {}
+  );
   const [selectedNft, setSelectedNft] = useState<MyNftToken>();
 
   const { address, isConnected } = useAccount();
@@ -50,6 +60,35 @@ export function MainSection() {
     }
   }, [client, address]);
 
+  const { data, isLoading: isLoadingAuctionStatus } = useReadContracts({
+    contracts: nfts.map((token) => ({
+      address: DUTCH_AUCTION_CONTRACT,
+      abi: DUTCH_AUCTION_ABI,
+      functionName: 'auctions',
+      args: [MORCHI_NFT_CONTRACT, token.tokenId],
+    })),
+    query: {
+      enabled: isConnected && nfts.length > 0,
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      const auctionStatus = data.reduce(
+        (acc: Record<string, boolean>, { result }: any) => {
+          if (result[0] === zeroAddress) return acc;
+          // startingTime > 0 && soldPrice === 0
+          acc[`${result[0]}-${String(result[1])}`] =
+            result[5] > 0 && result[7] === BigInt(0);
+          return acc;
+        },
+        {}
+      );
+
+      setAuctionStatus(auctionStatus);
+    }
+  }, [data]);
+
   useEffect(() => {
     loadMorchiNFTs();
   }, [loadMorchiNFTs]);
@@ -59,10 +98,22 @@ export function MainSection() {
     setIsCreateModalOpen(true);
   }
 
+  async function shareOnTwitter(nft: MyNftToken) {
+    const text = 'Want a Morchi NFT, check out this Auction!';
+    const url = `https://viewer.tokenscript.org/?chain=${CHAIN_ID}&contract=${MORCHI_NFT_CONTRACT}&tokenId=${nft.tokenId}&scriptId=7738_X#card=Buy`;
+    const twitterLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      text
+    )}&url=${encodeURIComponent(url)}`;
+
+    window.open(twitterLink, '_blank');
+  }
+
   function onCreateModalClose() {
     setIsCreateModalOpen(false);
     setSelectedNft(undefined);
   }
+
+  const isLoading = isLoadingNFTs || isLoadingAuctionStatus;
 
   return (
     <main className='flex-grow container mx-auto px-4 py-12'>
@@ -81,7 +132,7 @@ export function MainSection() {
           Your Morchi NFTs
         </h3>
         {isConnected ? (
-          isLoadingNFTs ? (
+          isLoading ? (
             <Loading />
           ) : (
             <>
@@ -93,6 +144,10 @@ export function MainSection() {
               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
                 {nfts.map((nft) => {
                   const metadata = nft.tokenMetadata! as MorchiMetadata;
+                  const onAuction =
+                    auctionStatus[
+                      `${MORCHI_NFT_CONTRACT}-${String(nft.tokenId)}`
+                    ];
 
                   return (
                     <Card
@@ -115,12 +170,22 @@ export function MainSection() {
                         />
                       </CardContent>
                       <CardFooter>
-                        <Button
-                          onClick={() => createAuction(nft)}
-                          className='w-full bg-[rgb(255,127,81)] text-white hover:bg-[rgb(255,150,110)] transition-colors'
-                        >
-                          Create Auction
-                        </Button>
+                        <div className='flex flex-col gap-2 w-full'>
+                          <Button
+                            onClick={() => createAuction(nft)}
+                            className='w-full bg-[rgb(255,127,81)] text-white hover:bg-[rgb(255,150,110)] transition-colors'
+                          >
+                            {onAuction ? 'Update' : 'Create'} Auction
+                          </Button>
+                          {onAuction && (
+                            <Button
+                              onClick={() => shareOnTwitter(nft)}
+                              className='w-full bg-[rgb(255,127,81)] text-white hover:bg-[rgb(255,150,110)] transition-colors'
+                            >
+                              Share on Twitter
+                            </Button>
+                          )}
+                        </div>
                       </CardFooter>
                     </Card>
                   );
